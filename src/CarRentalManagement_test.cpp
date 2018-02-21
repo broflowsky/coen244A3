2d673e04dd28076233ac7216115786d556826160
neboskrebnut <ivanov.arseniy@gmail.com>
2018-02-21 16:47:18 -0500

    few additions for VS

--- src/CarRentalManagement_test.cpp
+++ src/CarRentalManagement_test.cpp
@@ -9,14 +9,27 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main(){
 
+#ifndef KEEP_LIVE					//class for keeping console open after the end of execution
+#define KEEP_LIVE
+class KeepAlive
+{
+public:
+	KeepAlive() {};
+	~KeepAlive() {										
+		std::cout << "\nPress enter to exit.";
+		std::cin.get();
+	};
+};
+#endif
+
+int main(){
+	KeepAlive k{};
 
 	CarRentalManagement *Coen244CarCompany = new CarRentalManagement();
 
 
 	cout<<"Car Rental Management System Driver";
-
 
 
 	return 0;
