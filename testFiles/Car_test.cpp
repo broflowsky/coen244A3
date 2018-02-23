75e7421c6ca5ae73c83c3539e03c7c33a5efeb92
broflowsky <v.puyfourcat@gmail.com>
2018-02-23 15:15:55 -0500

    Car_test and customer_test are pretty much done

--- testFiles/Car_test.cpp
+++ testFiles/Car_test.cpp
@@ -13,9 +13,32 @@ using namespace std;
 
 int main(){
 
+
+	cout<<"\tCar Driver\n";
+
+	//Creating Car instances
 	Car*c1 = new Car();
 	Car*c2 = new Car("Luxe",1000);
 	Car*c3 = new Car(*c2);
-	cout <<*c1<<endl<<*c2<<endl<<*c3;
+
+
+	//Printing cars info
+	cout<<*c1<<endl
+		<<*c2<<endl
+		<<*c3<<endl;
+
+	//Using setters
+	c1->setID(100);
+	c1->setAvailability(false);
+	c1->setType("luxe");
+
+	//Printing info using the getters
+	cout<<"\nPrinting info updated car\n";
+	cout<<"\nID:\t"				<<c1->getID()
+		<<"\nType:\t"			<<c1->getType()
+		<<"\nAvailability:\t"	<<(c1->getAvailability()?"available":"rented");
+
+
 	return 0;
 }
+
